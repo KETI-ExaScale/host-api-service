70f223c2fcd01642df25864de9e859759aabcbf4
hth0919 <hth0919@gmail.com>
2023-10-20 11:16:43 +0900

    install script add

--- 1.build.sh
+++ 1.build.sh
@@ -10,7 +10,7 @@ go build -o build/bin/host-api-service cmd/main.go
 rm -rf /usr/lib/systemd/system/host-api-service.service
 rm -rf /usr/bin/local/.keti
 cp --force build/host-api-service.service /usr/lib/systemd/system/host-api-service.service
-#mkdir /usr/bin/local
+mkdir /usr/bin/local
 mkdir /usr/bin/local/.keti
 cp --force build/bin/host-api-service /usr/bin/local/.keti/host-api-service
 
